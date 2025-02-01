ddebcaa095b4974df5f87c15023839f1d1fd255f
leon-ronaldo <ronaldoleon029@gmail.com>
2025-02-01 13:09:32 +0530

    create/superuser, admin, student login pages completed

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,20 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import "./App.css";
-import Login from "./pages/Login";
+import { AppPages } from "./utils/constants";
+
+import './assets/fonts/BrittiSans/BrittiSans-Regular.otf'
+import StudentLogin from "./pages/StudentLogin";
+import SuperAdminLogin from "./pages/SuperAdminLogin";
+import AdminLogin from "./pages/AdminLogin";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Login />} />
+        <Route path={"/"} element={<StudentLogin />} />
+        <Route path={AppPages.studentLogin.route} element={<StudentLogin />} />
+        <Route path={AppPages.superUserLogin.route} element={<SuperAdminLogin />} />
+        <Route path={AppPages.adminLogin.route} element={<AdminLogin />} />
       </Routes>
     </BrowserRouter>
   );
